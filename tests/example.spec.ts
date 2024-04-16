059087352f17ffbe8d22e5dd4cd97c0e59733446
xinyi.xie@duke.edu <xx98@duke.edu>
2024-04-16 10:23:08 -0400

    basic e2e test

---
+++ tests/example.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test contact seller', async ({ page }) => {
+  await page.goto('http://localhost:3001/');
+  await page.getByRole('button', { name: 'Outdoors' }).click();
+  await page.getByRole('img', { name: 'Mountain Bike image' }).click();
+  await expect(page.getByRole('button', { name: 'Contact Seller' })).toBeVisible();
+  await page.getByRole('button', { name: 'Contact Seller' }).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.locator('div').filter({ hasText: /^phoneNumber: 987-654-3210 Copy$/ }).getByRole('button').click();
+});
